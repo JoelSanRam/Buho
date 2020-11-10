9d0d5130632d079ee5448bd0776834fc8305ac0d
urielspv <urielperez9781@gmail.com>
2020-11-10 17:25:32 -0600

    Actualizacion form y Slide

--- contact.php
+++ contact.php
@@ -1,74 +1,42 @@
 <?php
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\SMTP;
-use PHPMailer\PHPMailer\Exception;
-
-require 'PHPMailer/Exception.php';
-require 'PHPMailer/PHPMailer.php';
-require 'PHPMailer/SMTP.php';
-
-$mail = new PHPMailer(true);
-
-// configure
-$from = 'hola@buho-solutions.com';
-$subject = 'Búho Solutions | Nuevo mensaje de mi página web';
-$fields = array('name' => 'Name', 'email' => 'Email', 'message' => 'Message'); // array variable name => Text to appear in the email
+$subject = 'Nuevo Mensaje de mi página web | Buho Solutions '; // Subject of your email
 $okMessage = '¡Mensaje enviado!, en breve nos pondremos en contacto contigo';
 $errorMessage = 'Ocurrió un error, Por favor intente nuevamente más tarde.';
+$to = 'urielperez9781@gmail.com';  //Recipient's E-mail
+$emailTo = $_REQUEST['email'];
 
+$name = $_REQUEST['name'];
+$email = $_REQUEST['email'];
 
-try
+$msg = $_REQUEST['message'];
+
+$email_from = $name.'<'.$email.'>';
+
+$headers = "";
+$headers .= "";
+$headers .= "De: ".$name.'<'.$email.'>'."\r\n"; // Sender's E-mail
+$headers .= "Return-Path:"."De:" . $email;
+
+$message= "";
+$message .= 'Nombre : ' . $name . "\n";
+$message .= 'E-mail : ' . $email . "\n";
+
+$message .= 'Mensaje : ' . $msg;
+
+if (@mail($to, $subject, $message))
 {
-    //Server settings
-    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
-    $mail->isSMTP();                                            // Send using SMTP
-    $mail->Host       = 'smtp.zoho.com';                    // Set the SMTP server to send through
-    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-    $mail->Username   = 'hola@buho-solutions.com';                     // SMTP username
-    $mail->Password   = '';                               // SMTP password
-    $mail->SMTPSecure = 'tls';         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
-    $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
-
-
-   
-
-    //Recipients
-    $mail->setFrom($from, 'Buho Solutions');
-    $mail->addAddress('urielperez9781@gmail.com', 'Buho Solutions');
-    /* $mail->addReplyTo('hola@buho-solutions.com', 'Informacion');
-    $mail->addCC('joel.sanchez.ramirez10@gmail.com');
-    $mail->addCC('samuel.servin@buho-solutions.com');
-    $mail->addCC('samservin@hotmail.com');
-    $mail->addCC('urielperez9781@gmail.com'); */
-
-    $emailText = "Nueve mensaje de formulario<br>=============================<br>";
-
-    foreach ($_POST as $key => $value) {
-
-        if (isset($fields[$key])) {
-            $emailText .= "$fields[$key]: $value <br>";
-        }
-    }
-
-
-    // Content
-    $mail->isHTML(true);                                
-    $mail->Subject = 'Buho Solutions | Nuevo mensaje de Formulario Contacto';
-    $mail->Body    = "<br>".$emailText;
-    if($mail->send()){
-        $responseArray = array('type' => 'success', 'message' => $okMessage);
-    }
-    
-    
+	// Transfer the value 'sent' to ajax function for showing success message.
+	//echo 'Mensaje Enviado, nos comunicaremos lo antes posible.';
+	 $responseArray = array('type' => 'success', 'message' => $okMessage);
+	
 }
-catch (\Exception $e)
+else
 {
-    $responseArray = array('type' => 'danger', 'message' => $errorMessage);
-
+	// Transfer the value 'failed' to ajax function for showing error message.
+	echo 'Ha ocurrido un error, intente más tarde.';
+	 $responseArray = array('type' => 'danger', 'message' => $errorMessage);
 }
 
-var_dump($_SERVER['HTTP_X_REQUESTED_WITH']);
-var_dump($_SERVER['HTTP_X_REQUESTED_WITH']);
 
 
 if (!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
@@ -79,5 +47,11 @@ if (!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQ
     echo $encoded;
 }
 else {
-    echo $responseArray['message'];
+      $encoded = json_encode($responseArray);
+
+    header('Content-Type: application/json');
+
+    echo $encoded;
 }
+
+?>
